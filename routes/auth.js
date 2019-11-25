586b42a4ee280f3ce6f58b2967ca696c25f32e43
sarahmacharia <sarahmac632@gmail.com>
2019-11-25 16:28:01 +0300

    node js api

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const { registerValidation } = require('../routes/validation');
+
+
+
+
+router.post('/register', async (req, res) => {
+    const { error } = registerValidation(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+
+    } catch (err) {
+        res.status(400).send(err);
+
+    }
+});
+
+
+module.exports = router;
